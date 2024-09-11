003ca82b5b11faf551901950d6256677a25bfbb7
Manuel Loaiza Vasquez <19366788+ManuelLoaizaV@users.noreply.github.com>
2024-09-11 22:38:35 +0000

    feat(models): Add Role and User tables

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,53 @@
+from datetime import datetime
+from enum import Enum
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class RoleName(str, Enum):
+    CLIENT = "client"
+    CHEF = "chef"
+    ADMIN = "admin"
+
+class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.Enum(RoleName), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f"<Role {self.name.value}>"
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name.value
+        }
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    role_id = db.Column(db.Integer, db.ForeignKey("role.id"), nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    phone_number = db.Column(db.String(20))
+    is_active = db.Column(db.Boolean, nullable=False, default=True)
+    profile_picture_url = db.Column(db.String(255))
+    hashed_password = db.Column(db.String(255), nullable=False)
+    salt = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
+
+    def __repr__(self):
+        return f"<User {self.email}>"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "role_id": self.role_id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "phone_number": self.phone_number,
+            "is_active": self.is_active,
+            "profile_picture_url": self.profile_picture_url,
+            "created_at": self.created_at
         }
