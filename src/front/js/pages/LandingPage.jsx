41281771a944a1f33dc20c1831a52e218dee1c06
Rossy Jasmin Palacin Nuñez <119419018+ROZZYNUEZ14@users.noreply.github.com>
2024-09-12 15:37:23 +0000

    Se grega el logo kusi

--- src/front/js/pages/LandingPage.jsx
+++ src/front/js/pages/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import landingImage_1 from "../../img/landingImage_1.png"
 import landingImage_2 from "../../img/landingImage_2.png"
+import logoKusi from "../../img/Kusi.png"
 import "../../styles/landingPage.css";
 
 export const LandingPage = () => {
@@ -11,6 +12,9 @@ export const LandingPage = () => {
                     <img className="img-fluid" src={landingImage_1} alt="Landing image 1" />
                 </div>
                 <div className="landing-content col-12 col-md-8 col-lg-10 d-flex flex-column justify-content-center align-items-center px-3 px-md-5">
+                    <div className="logo-container w-100 mb-5 pb-3 pb-md-5 d-flex justify-content-center align-items-center" >
+                        <img className="img-fluid logo-kusi w-25 w-lg-75" style={{maxWidth:"250px"}} src={logoKusi} alt="Logo Kusi"/>
+                    </div>
                     <div className="landing-text text-center mb-4 mb-md-5">
                         <h1 className="px-4 px-md-1" style={{fontSize:"6vw"}}>Descubre los auténticos sabores del Perú en cada bocado</h1> 
                     </div>
