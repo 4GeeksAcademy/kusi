fcbda4b34e00fa5546a081693bf1f7871c0ac472
Manuel Loaiza Vasquez <19366788+ManuelLoaizaV@users.noreply.github.com>
2024-09-12 17:59:06 +0000

    refactor(models): Add __repr__ and serialize methods to schemas

--- src/api/models.py
+++ src/api/models.py
@@ -64,6 +64,15 @@ class OrderStatus(db.Model):
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)    
 
+    def __repr__(self):
+        return f"<OrderStatus {self.name.value}>"
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name.value
+        }
+
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     client_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
@@ -73,12 +82,35 @@ class Order(db.Model):
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
 
+    def __repr__(self):
+        return f"<Order {self.id}>"
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "client_id": self.client_id,
+            "status_id": self.status_id,
+            "grand_total": self.grand_total,
+            "special_instructions": self.special_instructions,
+            "created_at": self.created_at
+        }
+
 class OrderDish(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     order_id = db.Column(db.Integer, db.ForeignKey("order.id"), nullable=False)
     dish_id = db.Column(db.Integer, db.ForeignKey("dish.id"), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
+
+    def __repr__(self):
+        return f"<OrderDish {self.id}>"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "order_id": self.order_id,
+            "dish_id": self.dish_id,
+        }
 
 class Dish(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -90,6 +122,20 @@ class Dish(db.Model):
     cooking_time = db.Column(db.Integer)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
+
+    def __repr__(self):
+        return f"<Dish {self.name}>"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "image_url": self.image_url,
+            "price": self.price,
+            "discount_percentage": self.discount_percentage,
+            "cooking_time": self.cooking_time
+        }
  
 class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -97,9 +143,28 @@ class Ingredient(db.Model):
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
 
+    def __repr__(self):
+        return f"<Ingredient {self.name}>"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
+
 class DishIngredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     dish_id = db.Column(db.Integer, db.ForeignKey("dish.id"), nullable=False)
     ingredient_id = db.Column(db.Integer, db.ForeignKey("ingredient.id"), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
+
+    def __repr__(self):
+        return f"<DishIngredient {self.id}>"
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "dish_id": self.dish_id,
+            "ingredient_id": self.ingredient_id
+        }
