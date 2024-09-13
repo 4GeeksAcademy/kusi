5d295837e9d576ca8ec1842cc5c97f088aee2675
Angel Crispin <155650252+AngelCrispin@users.noreply.github.com>
2024-09-13 15:51:09 +0000

    docs(api): Add swagger

--- src/app.py
+++ src/app.py
@@ -2,14 +2,16 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for, send_from_directory
+from flask import Flask, jsonify, send_from_directory
 from flask_migrate import Migrate
-from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
 from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from api.swagger import setup_swagger
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
 # from models import Person
 
@@ -18,6 +20,11 @@ static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+#JWT
+
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
@@ -36,6 +43,9 @@ setup_admin(app)
 
 # add the admin
 setup_commands(app)
+
+# add swagger
+setup_swagger(app)
 
 # Add all endpoints form the API with a "api" prefix
 app.register_blueprint(api, url_prefix='/api')
