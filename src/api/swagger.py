970095514717dd2168b03c34623046eebc42cd49
Manuel Loaiza Vasquez <19366788+ManuelLoaizaV@users.noreply.github.com>
2024-09-13 22:21:45 +0000

    chore(swagger): Rename docs and reset migrations

--- src/api/swagger.py
+++ src/api/swagger.py
@@ -5,13 +5,13 @@ from api.namespace.hello_auth import api as hello_auth
 from api.namespace.auth import api as auth
 
 def setup_swagger(app):
-
     api = Api(app,
         title="Kusi API Documentation",
         description="Endpoints and Models",
         version="1.0",
-        doc="/swagger/",
-        validate=True)
+        doc="/docs",
+        validate=True
+    )
     
     api.add_namespace(hello)
     api.add_namespace(auth)
