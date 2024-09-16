dcb0177066074d4cdf79e826b48e0fa914c84c04
JhoelSand <126299517+Jhoel1999@users.noreply.github.com>
2024-09-16 12:31:06 +0000

    Vizualizar detalle del plato

--- src/front/js/component/modalDetalle.jsx
+++ src/front/js/component/modalDetalle.jsx
@@ -23,22 +23,22 @@ export const ModalDetalle = ({ id, imgSrc, title, description, ingredients }) =>
                             className="img-fluid w-100 h-auto ratio ratio-1x1" 
                             style={{ maxHeight: '300px', objectFit: 'cover', borderTopLeftRadius: '20px', borderTopRightRadius: '20px' }} 
                         />
-                        <button type="button" className="btn-close btn-light rounded-circle position-absolute top-0 end-0 m-3" data-bs-dismiss="modal" aria-label="Close"></button>
-                        <div className="row d-flex m-3">
-                            <div className="col-8 d-inline">
+                        <button type="button" className="btn-close bg-light rounded-circle position-absolute top-0 end-0 m-3" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <div className="row d-flex mt-3 mx-3">
+                            <div className="col">
                                 <h4>{title}</h4>
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam voluptatem, tempora molestias deleniti expedita dicta veritatis{description}</p>
                                 <p>Ingredientes: Lorem ipsum dolor sit amet consectetur adipisicing elit. At illum fuga quibusdam laborum. Mollitia quo recusandae{ingredients}</p>
                             </div>
-                            <div className="col-4 d-flex justify-content-center align-items-center mt-4 mb-3">
-                            <button className="btn" style={{ color: '#F44322', fontSize: '24px' }} onClick={handleDecrement}>
-                                <i className="fa-solid fa-circle-minus"></i>
-                            </button>
-                            <span className="mx-3" style={{ fontSize: '20px' }}>{amount}</span>
-                            <button className="btn" style={{ color: '#F44322', fontSize: '24px' }} onClick={handleIncrement}>
-                                <i className="fa-solid fa-circle-plus"></i>
-                            </button>
                         </div>
+                        <div className="col-4 d-flex justify-content-center align-items-center mb-2">
+                                <button className="btn" style={{ color: '#F44322', fontSize: '24px' }} onClick={handleDecrement}>
+                                    <i className="fa-solid fa-circle-minus"></i>
+                                </button>
+                                <span className="mx-3" style={{ fontSize: '20px' }}>{amount}</span>
+                                <button className="btn" style={{ color: '#F44322', fontSize: '24px' }} onClick={handleIncrement}>
+                                    <i className="fa-solid fa-circle-plus"></i>
+                                </button>
                         </div>
                         <button type="button" className="btn rounded-pill px-4" style={{ color: 'white', background: '#F44322' }}>Agregar</button>
                     </div>
