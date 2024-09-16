e2da10e151a186ddfaf6e02fd88a439db58422aa
Rossy Jasmin Palacin Nuñez <119419018+ROZZYNUEZ14@users.noreply.github.com>
2024-09-16 14:42:02 +0000

    Se trabaja en los stilos

--- src/front/js/pages/Profile.jsx
+++ src/front/js/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import userImg from '../../assets/images/user.png';
+import '../../styles/profile.css'
 
 export const Profile = () => {
 
@@ -69,13 +70,13 @@ export const Profile = () => {
         <div className="d-flex justify-content-center align-items-center vh-100">
             <div className="container text-center">
                 <div className="row d-flex justify-content-center align-items-center  gap-5">
-                    <div className="col-12 h-auto bg-primary d-flex justify-content-center align-items-center p-2">
-                        <div className="bg-danger px-2 px-lg-5 py-3 w-50 d-flex flex-column flex-md-row justify-content-center align-items-center">
+                    <div className="col-12 profile-box h-auto px-2 bg-primary w-100 d-flex justify-content-center align-items-center">
+                        <div className="profile-box-text bg-danger px-0 px-lg-5 d-flex flex-column flex-md-row justify-content-center align-items-center">
                             <div className="w-50 w-lg-25 d-flex justify-content-center align-items-center">
                                 <img src={userImg} className="w-100"/>
                             </div>
                             <div className="w-100 d-flex flex-column justify-content-center text-center text-md-start ps-md-2 ps-lg-5">
-                               <h5>{userData.name}</h5>
+                               <h4>{userData.name}</h4>
                                {
                                 userRole === "admin" || userRole === "chef" ? (
                                     <p>{userData.position}</p>
@@ -85,31 +86,36 @@ export const Profile = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="col-12 bg-secondary d-flex justify-content-center">
-                        <div className="w-100 d-flex justify-content-center">
-                            <div className="w-50 bg-success">
-                                <h1>Hola</h1>
-                                <div>
-                                    <div>
-                                      <h5>Nombre</h5>
-                                        <input type="text" defaultValue={userData.name}/>  
+                    <div className="col-12 bg-secondary px-2 d-flex justify-content-center">
+                        <div className="group-profile w-100 d-flex justify-content-center">
+                            <div className="group-profile-text bg-success">
+                                <h4 className="text-start">Información personal</h4>
+                                <div className="d-flex justify-content-evenly align-items-start gap-5">
+
+                                    <div className="d-flex flex-column justify-content-center w-100">
+                                        <div className="w-50 ms-5">
+                                            <h5>Nombre</h5>
+                                            <input className="w-100" type="text" defaultValue={userData.name}/>  
+                                        </div>
+
+                                        <div>
+                                            <h5>Correo</h5>
+                                            <input type="text" defaultValue={userData.email}/>  
+                                        </div>
+
                                     </div>
 
-                                    <div>
-                                      <h5>Correo</h5>
-                                        <input type="text" defaultValue={userData.email}/>  
-                                    </div>
+                                    <div className="d-flex flex-column justify-content-center">
+                                        <div>
+                                            <h5>Teléfono</h5>
+                                            <input type="number" defaultValue={userData.phone}/>  
+                                        </div>
 
-                                    <div>
-                                      <h5>Teléfono</h5>
-                                        <input type="number" defaultValue={userData.phone}/>  
+                                        <div>
+                                            <h5>Contraseña</h5>
+                                            <input type="password" defaultValue={userData.password}/>  
+                                        </div>
                                     </div>
-
-                                    <div>
-                                      <h5>Contraseña</h5>
-                                        <input type="password" defaultValue={userData.password}/>  
-                                    </div>
-
                                 </div>
                             </div>
                         </div>
