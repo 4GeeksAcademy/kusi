ee1aad2c93074dd246f03097feac9e85432c39e5
Manuel Loaiza Vasquez <19366788+ManuelLoaizaV@users.noreply.github.com>
2024-09-16 19:29:44 +0000

    refactor(navbar): Minor changes and TODO comments

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import injectContext from './store/appContext';
 import LandingPage from './pages/LandingPage.jsx';
-import { Navbar } from './component/Navbar.jsx';
 
 const Layout = () => {
     const basename = process.env.BASENAME || '';
@@ -10,7 +9,6 @@ const Layout = () => {
     return (
         <div>
             <BrowserRouter basename={basename}>
-            <Navbar/>
                 <Routes>
                     <Route element={<LandingPage />} path="/" />
                     <Route element={<h1>Not found!</h1>} />
