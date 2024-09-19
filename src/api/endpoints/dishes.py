ec9b1c1f6841a23aa556ebcfe6fe42e02df85a7a
Manuel Loaiza Vasquez <19366788+ManuelLoaizaV@users.noreply.github.com>
2024-09-19 20:27:57 +0000

    feat(dishes): Fetch dish by ID

--- src/api/endpoints/dishes.py
+++ src/api/endpoints/dishes.py
@@ -1,8 +1,9 @@
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import jwt_required
 from flask_restx import Resource, fields
-from api.models import Dish
+from api.models import db, Dish, DishIngredient, Ingredient
 from api.namespaces import dishes_namespace
+from api.utils import InvalidAPIUsage
 
 bcrypt = Bcrypt()
 
@@ -44,41 +45,31 @@ class DishList(Resource):
         except Exception as e:
             return { "message": str(e) }, 500
 
-# @dishes_namespace.response(201, 'Successful')
-# @dishes_namespace.response(500, 'Server error')
-# @dishes_namespace.route('/')
-# class DishAPI(Resource):
-#     method_decorators = [jwt_required()]
-#     @dishes_namespace.doc(security="jsonWebToken")
-#     @dishes_namespace.marshal_list_with(dish_model)
-#     def get(self):
-#         '''Get all dishes'''
-#         try:
-#             all_dishes = Dish.query.all()
-#             return all_dishes,201
-#         except Exception as error:
-#             return str(error),500
-        
+@dishes_namespace.response(200, "OK")
+@dishes_namespace.response(404, "Dish not found")
+@dishes_namespace.response(500, "Internal server error")
+@dishes_namespace.route("/<int:dish_id>")
+class DishCRUD(Resource):
+    method_decorators = [jwt_required()]
+    @dishes_namespace.doc(security="jsonWebToken")
+    def get(self, dish_id):
+        """Fetches the dish with specified ID."""
+        dish = Dish.query.filter_by(id=dish_id).one_or_none()
+        if dish is None:
+            raise InvalidAPIUsage(f"Dish {dish_id} not found.", 404)
 
-# @dishes_namespace.route('/<id>')
-# @dishes_namespace.param('id', 'Dish identifier')
-# @dishes_namespace.response(201, 'Successful')
-# @dishes_namespace.response(404, 'Dish not found')
-# @dishes_namespace.response(500, 'Server error')
-# class DishAPI(Resource):
-#     method_decorators = [jwt_required()]
-#     @dishes_namespace.doc(security="jsonWebToken")
-#     @dishes_namespace.marshal_with(dish_model)
-#     def get(self, id):
-#         '''Get dish by id'''
-#         try:
-#             dish = Dish.query.filter_by(id=id).first()
-#             if not dish:
-#                 return "Dish not found",404
-#             return dish
-#         except Exception as error:
-#             return str(error),500
-        
+        try:
+            ingredients = list(
+                map(
+                    lambda ingredient: ingredient.serialize(),
+                    db.session.query(Ingredient).join(DishIngredient).filter(DishIngredient.dish_id == dish_id).all()
+                )
+            )
+            response = dish.serialize()
+            response["ingredients"] = ingredients
+            return response, 200
+        except Exception as e:
+            return { "message": str(e) }, 500
 
 # @dishes_namespace.route('/<id>/ingredients')
 # @dishes_namespace.param('id', 'Dish identifier')
