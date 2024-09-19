2471dc663b0acf5434c851d825bb69cd34c7395a
Rossy Jasmin Palacin Nuñez <119419018+ROZZYNUEZ14@users.noreply.github.com>
2024-09-19 23:11:20 +0000

    Se modifican los estilos de la lista de ordenes

--- src/front/js/pages/Orders.jsx
+++ src/front/js/pages/Orders.jsx
@@ -1,27 +1,15 @@
 import React, { useEffect, useState, useContext } from "react";
 import { Context } from "../store/appContext";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHourglassStart, faFire, faBan, faUtensils } from '@fortawesome/free-solid-svg-icons';
+import { Navbar } from "../component/Navbar.jsx";
+import "../../styles/orders.css"
 
 export const Orders = () => {
     const { store, actions } = useContext(Context);
     const [ordersData, setOrdersData] = useState([]);
 
     useEffect(() => {
-        // const fetchData = async () => {
-        //     await actions.orders();
-            
-        //     const ordersWithClientInfo = await Promise.all(
-        //         store.dataOrders.map(async (order) => {
-        //             const clientData = await actions.getUsersById(order.);
-        //             return {
-        //                 ...order,
-        //                 clientName: clientData?.name || "Desconocido"
-        //             };
-        //         })
-        //     );
-
-        //     setOrdersData(ordersWithClientInfo);
-        // };
-        // fetchData();
         const fetchData = async () => {
             await actions.orders();
             setOrdersData(store.dataOrders);
@@ -29,131 +17,88 @@ export const Orders = () => {
         fetchData();
     }, []);
 
-    // const Status = Object.freeze({
-    //     PENDING: "pending",
-    //     IN_PROGRESS: "in progress",
-    //     COMPLETED: "completed",
-    //     CANCELLED: "cancelled"
-    // })
-    const Status = Object.freeze({
+    const Status = Object.freeze ({
         PENDING: 1,
         IN_PROGRESS: 2,
         COMPLETED: 3,
         CANCELLED: 4
-    })
+    });
 
     const orderStatus = {
         [Status.PENDING]: "Pendiente",
         [Status.IN_PROGRESS]: "En progreso",
         [Status.COMPLETED]: "Completado",
         [Status.CANCELLED]: "Cancelado"
-    }
-    
-    
+    };
+
+    const handleStatusChange = (id) => {
+        setOrdersData(prevOrders => 
+            prevOrders.map(order => {
+                if (order.id === id) {
+                    const nextStatus = {
+                        [Status.PENDING]: Status.IN_PROGRESS,
+                        [Status.IN_PROGRESS]: Status.COMPLETED,
+                        [Status.COMPLETED]: Status.COMPLETED,
+                        [Status.CANCELLED]: Status.CANCELLED
+                    };
+                    return { ...order, status_id: nextStatus[order.status_id] };
+                }
+                return order;
+            })
+        );
+    };
 
     return (
         <div>
-            <div className="container text-center">
-                <div className="row">
-                    <div className="col-2 bg-primary">
-                        <h5>N° ORDEN</h5>
-                        <div className="row">
-                            {ordersData.length > 0 ? (
-                                ordersData.map((item) => (
-                                    <div className="col-12" key={item.id}>
-                                        {item.id}
-                                    </div>
-                                ))
-                            ) : (
-                                <p>No hay órdenes que mostrar</p>
-                            )}
-                        </div>
-                    </div>
-
-                    <div className="col-2 bg-secondary">
-                        <h5>FECHA</h5>
-                        <div className="row">
-                            {ordersData.length > 0 ? (
+            <Navbar/>
+        
+            <div className="container d-flex justify-content-center align-items-center vh-100">
+                {ordersData.length > 0 ? (
+                    <table className="table table-hover text-center border-danger">
+                        <thead >
+                            <tr>
+                                <th>N° ORDEN</th>
+                                <th>FECHA</th>
+                                <th>CLIENTE</th>
+                                <th>TOTAL</th>
+                                <th>ESTADO</th>
+                                <th>ACCIÓN</th>
+                            </tr>
+                        </thead>
+                        <tbody>
                                 ordersData.map((item) => {
-                                    const registerClient = new Date(item.created_at);
-                                    const formattedDate = registerClient.toLocaleDateString('es-ES');
-                                    return (
-                                        <div className="col-12" key={item.id}>
-                                            {formattedDate}
-                                        </div>
-                                    );
+                                    <tr key={item.id} >
+                                        <td>#{item.id}</td>
+                                        <td>{new Date(item.created_at).toLocaleDateString('es-ES')}</td>
+                                        <td>{item.user?.name || "Desconocido"}</td>
+                                        <td>S/.{item.grand_total.toFixed(2)}</td>
+                                        <td>
+                                            <p
+                                                className={orderStatus[item.status_id] === "Pendiente" ? "border border-primary py-1 text-primary " : orderStatus[item.status_id] === "En progreso" ? "border border-warning py-1 text-warning" : orderStatus[item.status_id] === "Completado" ? "border border-success py-1 text-success" : "border border-danger py-1 text-danger" }
+                                                style={{width:"100%", textAlign:"center", borderRadius:"50px", backgroundColor:"white"}}
+                                            >
+                                                    {orderStatus[item.status_id] || "Desconocido"}
+                                            </p>
+                                            
+                                        </td>
+                                        <td>
+                                            <button
+                                                className={item.status_id === Status.PENDING ? "btn btn-primary" : item.status_id === Status.IN_PROGRESS ? "btn btn-warning" : item.status_id === Status.COMPLETED ? "btn btn-success" : "btn btn-danger"}
+                                                
+                                                onClick={() => handleStatusChange(item.id)}
+                                                disabled={item.status_id === Status.COMPLETED || item.status_id === Status.CANCELLED}
+                                            >
+                                                {item.status_id === Status.PENDING ? <FontAwesomeIcon icon={faHourglassStart} /> : item.status_id === Status.IN_PROGRESS ? <FontAwesomeIcon icon={faFire} /> : item.status_id === Status.COMPLETED ? <FontAwesomeIcon icon={faUtensils}/> : <FontAwesomeIcon icon={faBan} /> }
+                                            </button>
+                                        </td>
+                                    </tr>
                                 })
-                            ) : (
-                                <p>No hay datos que mostrar</p>
-                            )}
-                        </div>
-                    </div>
-
-                    <div className="col-2 bg-danger">
-                        <h5>CLIENTE</h5>
-                        <div className="row">
-                            {ordersData.length > 0 ? (
-                                ordersData.map((item) => (
-                                    <div className="col-12" key={item.id}>
-                                        {item.user?.name || "Desconocido"}
-                                    </div>
-                                ))
-                            ) : (
-                                <p>No hay datos que mostrar</p>
-                            )}
-                        </div>
-                    </div>
-
-                    <div className="col-2 bg-success">
-                        <h5>TOTAL</h5>
-                        <div className="row">
-                            {ordersData.length > 0 ? (
-                                ordersData.map((item) => (
-                                    <div className="col-12" key={item.id}>
-                                        S/.{item.grand_total.toFixed(2)}
-                                    </div>
-                                ))
-                            ) : (
-                                <p>No hay datos que mostrar</p>
-                            )}
-                        </div>
-                    </div>
-
-                    <div className="col-2 bg-warning">
-                        <h5>ESTADO</h5>
-                        <div className="row">
-                            {ordersData.length > 0 ? (
-                                ordersData.map((item) => (
-                                    <div className="col-12" key={item.id}>
-                                        {orderStatus[item.status_id] || "Desconocido"}
-                                    </div>
-                                ))
-                            ) : (
-                                <p>No hay datos que mostrar</p>
-                            )}
-                        </div>
-                    </div>
-
-                    <div className="col-2 bg-info">
-                        <h5>ACCIÓN</h5>
-                        <div className="row">
-                            {ordersData.length > 0 ? (
-                                ordersData.map((item) => (
-                                    <div className="col-12" key={item.id}>
-                                        <div className="form-check">
-                                            <input className="form-check-input" type="checkbox" id={`checkbox-${item.id}`} />
-                                            <label className="form-check-label" htmlFor={`checkbox-${item.id}`}>
-                                                Checkbox
-                                            </label>
-                                        </div>
-                                    </div>
-                                ))
-                            ) : (
-                                <p>No hay datos que mostrar</p>
-                            )}
-                        </div>
-                    </div>
-                </div>
+                        </tbody>
+                    </table>
+                    ):(
+                        <p>No hay datos que mostrar</p>
+                    )
+                }
             </div>
         </div>
     );
