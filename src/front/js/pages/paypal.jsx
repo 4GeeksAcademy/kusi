c244ec8a6bd2ef6a98a39dbbbf6567742c7e4fd9
octavio ruben flores ramirez <ruben94fr@hotmail.com>
2024-09-19 22:53:28 +0000

    se modifico value

--- src/front/js/pages/paypal.jsx
+++ src/front/js/pages/paypal.jsx
@@ -33,7 +33,7 @@ export const Paypal = () => {
                                     return actions.order.create({
                                         purchase_units:[{
                                             amount:{
-                                                value: 20,  // store.order.grand_total
+                                                value:  store.order.grand_total
                                             }
                                         }]
                                     })
