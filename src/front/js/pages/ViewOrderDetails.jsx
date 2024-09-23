808e00eb5a5b88e5ec542a379f1e355e46a97dee
Rossy Jasmin Palacin Nuñez <119419018+ROZZYNUEZ14@users.noreply.github.com>
2024-09-23 02:24:42 +0000

    Se sube la actualizacion del Orders

--- src/front/js/pages/ViewOrderDetails.jsx
+++ src/front/js/pages/ViewOrderDetails.jsx
@@ -46,26 +46,32 @@ export const ViewOrderDetails = () => {
         [Status.CANCELLED]: "Cancelado"
     };
     return(
-        <div className="d-flex flex-column justify-content-center align-items-center">
-            <div className="card text-center w-50 h-auto">
-                <h2 className="card-header">TICKET DE ORDEN</h2>
+        <div className="d-flex flex-column justify-content-center align-items-center p-3"> 
+            <div className="card text-start w-50 h-auto d-flex flex-column justify-content-center">
+                <h2 className="card-header text-center p-4">N° Orden #{orderDetails?.id || "No disponible"}</h2>
                 <div className="card-body">
-                    <h5>N° Orden #{orderDetails?.id || "No disponible"}</h5>
                     <h5 className="card-title">Cliente: {orderDetails?.client?.name || "No disponible"}</h5>
                     <h5 className="card-title">Email: {orderDetails?.client?.email || "No disponible"}</h5>
                     <h5 className="card-title">Teléfono: {orderDetails?.client?.phone_number || "No disponible"}</h5>
-                    <h5>Fecha y hora: {new Date(orderDetails?.created_at).toLocaleDateString('es-ES')}</h5>
+                    <hr></hr>
+                    <h5>Fecha: {new Date(orderDetails?.created_at).toLocaleDateString('es-ES')}</h5>
                     <h5>Hora: {new Date(orderDetails?.created_at).toLocaleTimeString()}</h5>
-                    ------------
+                    <hr></hr>
                     <h5>Estado de la orden: {orderStatus[orderDetails?.status_id]}</h5>
-                    ------------
+                    <hr></hr>
                     <h5>Platos</h5>
                     {orderDetails.dishes && orderDetails.dishes.length > 0 ? (
                         orderDetails.dishes.map((dish, index) => (
                             <p key={index}>{dish.name}</p>
                         ))
                     ):(<p>No hay datos que mostrar</p>)}
-                    <p className="card-text"></p>
+                    <p className="text-end">S/.{orderDetails.grand_total}</p>
+                    <hr></hr>
+                    <div>
+                       <h5>Notas del pedido:</h5>
+                    </div>
+                    <hr></hr>
+                    <button type="button" class="btn btn-primary">Primary</button>
                 </div>
             </div>
         </div>
